b5f3e7226c0ace2a0f352d02496a80485900507d
Ohad Orbach <ohadorbach92@gmail.com>
2022-12-10 13:12:00 +0200

    update

--- main.py
+++ main.py
@@ -3,24 +3,9 @@ import math
 from Set import *
 
 
-def rotation_matrix(axis, theta):
-    """
-    Return the rotation matrix associated with counterclockwise rotation about
-    the given axis by theta radians.
-    """
-    axis = np.asarray(axis)
-    axis = axis / math.sqrt(np.dot(axis, axis))
-    a = math.cos(theta / 2.0)
-    b, c, d = -axis * math.sin(theta / 2.0)
-    aa, bb, cc, dd = a * a, b * b, c * c, d * d
-    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
-
-    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
-                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
-                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
-
-
 def main():
+    iterations = 6
+    cost_list = []
     Q1 = LineR3(np.array([0, 0, 0], dtype=np.float64), np.array([1, 2, 1], dtype=np.float64))
     Q2 = LineR3(np.array([0, 0, 0], dtype=np.float64), np.array([3, 4, 5], dtype=np.float64))
     Q3 = LineR3(np.array([0, 0, 0], dtype=np.float64), np.array([0, 3, 3], dtype=np.float64))
@@ -40,18 +25,43 @@ def main():
     theta = math.radians(5)
     R = rotation_matrix(axis, theta)
     t = np.array([5, -4, 2], dtype=np.float64)
+    t = t / np.linalg.norm(t)
 
     Set2.R_t_Set(R, t)
     Set2.noise()
 
-    for i in range(5):
+    for i in range(iterations):
         Set1.closest_points_set(Set2)
         dist = Set1.distance(Set2)
+        cost_list.append(dist)
         Set1.plot3d(Set2, title="sum of distances " + str(dist))
         _, R, t = Set2.best_fit_transform(Set1)
         R = np.around(R, decimals=2)
         t = np.around(t, decimals=2)
         Set2.R_t_Set(R, t)
+
+    plt.plot(list(range(iterations)), cost_list, '-r')
+    plt.title("cost")
+    plt.xlabel("sum of distances")
+    plt.ylabel("iterations")
+    plt.show()
+
+
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    axis = np.asarray(axis)
+    axis = axis / math.sqrt(np.dot(axis, axis))
+    a = math.cos(theta / 2.0)
+    b, c, d = -axis * math.sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+
+    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
 
 
 if __name__ == '__main__':
